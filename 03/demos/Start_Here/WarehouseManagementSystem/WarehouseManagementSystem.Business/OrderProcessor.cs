698efafae54f5d40ec60566b3984f41491cafe6c
Tidiane Diallo <diallotidiane014@gmail.com>
2023-09-12 13:09:36 +0000

    invoke delegate and return type delegate

---
+++ 03/demos/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem.Business/OrderProcessor.cs
@@ -0,0 +1,29 @@
+﻿using WarehouseManagementSystem.Domain;
+
+namespace WarehouseManagementSystem.Business
+{
+    public class OrderProcessor
+    {
+        public delegate bool OrderInitialized(Order order );
+        public delegate void ProcessCompleted( Order order);
+        public OrderInitialized OnOrderInitialized { get; set; }
+
+        private void Initialize(Order order)
+        {
+            ArgumentNullException.ThrowIfNull(order);
+
+            if(OnOrderInitialized?.Invoke(order) == false)
+            {
+                throw new ArgumentException($"couldn't initialize {order.OrderNumber}");
+            }
+        }
+
+        public void Process(Order order, ProcessCompleted OnCompleted = default)
+        {
+
+            Initialize(order);
+
+            OnCompleted?.Invoke(order );
+        }
+    }
+}
---
+++ 03/demos/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem/Program.cs
@@ -0,0 +1,39 @@
+﻿using WarehouseManagementSystem.Business;
+using WarehouseManagementSystem.Domain;
+
+var order = new Order
+{
+    LineItems = new[]
+    {
+        new Item{ Name = "PS1",Price = 50},
+        new Item{ Name = "PS2",Price = 60},
+        new Item{ Name = "PS3",Price = 70},
+        new Item{ Name = "PS4",Price = 80}
+    }
+};
+
+/*var processor = new OrderProcessor
+{
+    OnOrderInitialized = SendMessageToWarehouse
+};
+processor.Process(order, SendCofirmationnEmail);*/
+
+OrderProcessor.OrderInitialized GetOnInitialized()
+{
+    return SendMessageToWarehouse;
+}
+
+//GetOnInitialized()(order);
+GetOnInitialized()?.Invoke(order);
+
+
+bool SendMessageToWarehouse(Order order)
+{
+    Console.WriteLine($"Please pack the ordre {order.OrderNumber}");
+    return true;
+}
+
+void SendCofirmationnEmail(Order order)
+{
+    Console.WriteLine($"order confirmation email {order.OrderNumber}");
+}
